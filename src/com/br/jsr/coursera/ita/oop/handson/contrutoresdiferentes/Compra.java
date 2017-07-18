3ba6b7fa021fa96008cd99512d5dd502e42f37a3
Jefferson Rago <jefferson.s.rago@gmail.com>
2017-07-18 17:55:27 -0300

    Adicionando tarefas do Hands On: - Classes e Objetos - Construtores diferentes na mesma classe

---
+++ src/com/br/jsr/coursera/ita/oop/handson/contrutoresdiferentes/Compra.java
@@ -0,0 +1,33 @@
+package com.br.jsr.coursera.ita.oop.handson.contrutoresdiferentes;
+
+public class Compra {
+	
+	int valorTotal;
+	int numeroParcelas;
+	
+	// a vista
+	public Compra(int valor) {
+		this.valorTotal = valor;
+		this.numeroParcelas = 1;
+	}
+	
+	// parcelado
+	public Compra(int qtdParcelas, int valorParcela) {
+		this.numeroParcelas = qtdParcelas;
+		this.valorTotal = valorParcela * qtdParcelas;
+	}
+	
+	public int getValorTotal() {
+		return this.valorTotal;
+	}
+	
+	public int getNumeroParcelas() {
+		return this.numeroParcelas;
+	}
+	
+	public int getValorParcela() {
+		return this.valorTotal / this.numeroParcelas;
+	}
+	
+
+}
---
+++ src/com/br/jsr/coursera/ita/oop/teste/TesteAbstract.java
@@ -0,0 +1,5 @@
+package com.br.jsr.coursera.ita.oop.teste;
+
+public abstract class TesteAbstract {
+
+}
