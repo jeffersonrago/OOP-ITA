33b818561710e2f3a5b9067ca83c408865b643f3
Jefferson Rago <jefferson.s.rago@gmail.com>
2017-07-22 00:38:44 -0300

    Adicionando teste do contrutor de compra

---
+++ src/com/br/jsr/coursera/ita/oop/handson/contrutoresdiferentes/TesteCompra.java
@@ -0,0 +1,25 @@
+package com.br.jsr.coursera.ita.oop.handson.contrutoresdiferentes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TesteCompra {
+
+	
+	@Test
+	public void compraAVista(){
+		Compra c = new Compra(500);
+		Assert.assertEquals(1, c.getNumeroParcelas());
+		Assert.assertEquals(500, c.getValorTotal());
+		Assert.assertEquals(500, c.getValorParcela());
+	}
+	
+	@Test
+	public void compraParcelado(){
+		Compra c = new Compra(4,250);
+		Assert.assertEquals(4, c.getNumeroParcelas());
+		Assert.assertEquals(1000, c.getValorTotal());
+		Assert.assertEquals(250, c.getValorParcela());
+	}
+	
+}
